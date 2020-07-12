9026ca33f3ec3dffa39565d7f940ae38cafaf543
PipiMelan <1793126995@qq.com>
2020-07-12 17:17:23 +0800

    4.The main function

---
+++ src/main/KotlinMAIN/main.kt
@@ -0,0 +1,17 @@
+
+
+fun main() {
+    // The output sentence
+    println("") //  There will be a line feed
+    print("");
+
+    //  Output Variable 输出变量
+    val name = "rose"
+    println(name)
+
+    //  String Template
+    println("My name is $name")
+
+    //There will be a combination like this when the output variables are consist of multiple parts
+    println("My name is $name and length is ${name.length}")
+}

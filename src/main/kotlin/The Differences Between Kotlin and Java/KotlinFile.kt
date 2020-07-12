f2c2fb4f6e5c329122808cba812398ae70198e6a
PipiMelan <1793126995@qq.com>
2020-07-12 17:08:12 +0800

    3.Nothing important

---
+++ src/main/kotlin/The Differences Between Kotlin and Java/KotlinFile.kt
@@ -0,0 +1,18 @@
+import javax.swing.Spring
+
+//We can put functions in this
+        //We can put attributes in this
+        //Class
+// These are all static existences
+val SPRING = 0
+val SUMMR = 1
+
+
+fun main() {
+//    useTool()
+    println(SUMMR)
+    println("It is $SPRING")
+    }
+//    fun useTool(){
+//        print(MyClass.SPRING)
+//        }
